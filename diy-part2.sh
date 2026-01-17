fd4a63e625d12a0cbab1b241bc78707ae675b52a
zihan <meet.jia@qq.com>
2026-01-17 17:21:13 +0800

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -14,3 +14,5 @@
 sudo apt install libfuse-dev
 rm -rf feeds/packages/lang/golang
 git clone https://github.com/sbwml/packages_lang_golang -b 24.x feeds/packages/lang/golang
+./scripts/feeds update -a
+./scripts/feeds install -a
